2160b4eeeb39fae94961ceb8752292089fa3f0a8
ramosgk <guirmalves@gmail.com>
2023-06-12 12:18:45 -0300

    first-commit

---
+++ to-do-list.js
@@ -0,0 +1,75 @@
+const inputElement = document.querySelector('.digitar-tarefa');
+const addTaskButton = document.querySelector('.button-add');
+
+const tasksContainer = document.querySelector('.tasks-container')
+
+const validateInput = () => {
+  if (inputElement.value.trim().length > 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const handleAddTask = () => {
+  const inputIsValid = validateInput();
+
+  if (!inputIsValid) {
+    return inputElement.classList.add("error");
+  }
+
+  const taskItemContainer = document.createElement('div')
+  taskItemContainer.classList.add('task-item')
+
+  const taskContent = document.createElement('p')
+  taskContent.innerText = inputElement.value;
+
+  taskContent.addEventListener("click", () => handleClick(taskContent))
+
+  const deleteItem = document.createElement('i')
+  deleteItem.classList.add("far")
+  deleteItem.classList.add("fa-trash-alt")
+
+  deleteItem.addEventListener("click", () => handleDeleteClick(taskItemContainer, taskContent))
+
+  taskItemContainer.appendChild(taskContent)
+  taskItemContainer.appendChild(deleteItem)
+
+  tasksContainer.appendChild(taskItemContainer)
+
+  inputElement.value = "";
+}
+
+const handleClick = (taskContent) => {
+  const tasks = tasksContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+    if (currentTaskIsBeingClicked) {
+      task.firstChild.classList.toggle("completed");
+    }
+  }
+}
+
+const handleDeleteClick = (taskItemContainer, taskContent) => {
+ const tasks = tasksContainer.childNodes;
+
+ for (const task of tasks) {
+  const currentTaskIsBeingClicked = task.firstChild.isSameNode(taskContent)
+  if (currentTaskIsBeingClicked) {
+    taskItemContainer.remove()
+  }
+ }
+}
+
+const handleInputChange = () => {
+  const inputIsValid = validateInput();
+
+  if (inputIsValid) {
+    return inputElement.classList.remove("error");
+  }
+}
+
+addTaskButton.addEventListener("click", () => handleAddTask());
+
+inputElement.addEventListener("change", () => handleInputChange());
